d189ae555779799c8e5efa025197cc535220e637
Francesco Adriani <prof.f.adriani@gmail.com>
2024-01-31 22:52:09 +0100

    Programma embrionale alla prima versione per fare analisi delle foto con attributi alt dei tag image contenenti parole speciali

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace Facebook_scraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkbox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 37);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(923, 707);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1410, 31);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(941, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Estrai le immagini filtrate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(942, 84);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(455, 660);
+            this.panel1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(1148, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(249, 31);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "potrebbe essere";
+            // 
+            // checkbox1
+            // 
+            this.checkbox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkbox1.AutoSize = true;
+            this.checkbox1.Location = new System.Drawing.Point(941, 71);
+            this.checkbox1.Name = "checkbox1";
+            this.checkbox1.Size = new System.Drawing.Size(69, 17);
+            this.checkbox1.TabIndex = 5;
+            this.checkbox1.Text = "ripulendo";
+            this.checkbox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1410, 756);
+            this.Controls.Add(this.checkbox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkbox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net;
+
+namespace Facebook_scraper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Url = new Uri(textBox1.Text);
+            webBrowser1.Refresh();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            String s = webBrowser1.Document.Body.InnerHtml;
+            Dictionary<string, string> altAttributes = ExtractAltAttributes(s);
+
+            Console.WriteLine("Attributi 'alt' estratti:");
+            foreach (var altAttribute in altAttributes)
+            {
+                Console.WriteLine(altAttribute.Value);
+            }
+
+            List < Tuple<string, string> > imagesDictionary = await GetImagesFromWebBrowserAsync(webBrowser1);
+            await DisplayImagesAndAltTextAsync(panel1, imagesDictionary, textBox2.Text);
+
+            panel1.Refresh();
+        }
+        static Dictionary<string, string> ExtractAltAttributes(string html)
+        {
+            Dictionary<string, string> altAttributes = new Dictionary<string, string>();
+            String prefix = "potrebbe essere";
+
+            // Utilizza un'espressione regolare per trovare tutti gli attributi 'alt' nelle immagini
+            Regex regex = new Regex($@"<img[^>]*alt=[""']{Regex.Escape(prefix)}([^""']+)[""'][^>]*>", RegexOptions.IgnoreCase);
+            MatchCollection matches = regex.Matches(html);
+
+            foreach (Match match in matches)
+            {
+                string altAttribute = "alt=\"";
+                string urlAttribute = "src=\"";
+                int startIndex = match.Value.IndexOf(altAttribute);
+                int startIndexu = match.Value.IndexOf(urlAttribute);
+
+                if (startIndex != -1 && startIndexu != -1)
+                {
+                    startIndex += altAttribute.Length;
+                    startIndexu += urlAttribute.Length;
+                    int endIndex = match.Value.IndexOf("\"", startIndex);
+                    int endIndexu = match.Value.IndexOf("\"", startIndexu);
+
+                    if (endIndex != -1 && endIndexu != -1)
+                    {
+                        altAttributes.Add(match.Value.Substring(startIndexu, endIndexu - startIndexu), match.Value.Substring(startIndex, endIndex - startIndex));
+                    }
+                }
+            }
+
+            return altAttributes;
+        }
+        private async Task<List<Tuple<string, string>>> GetImagesFromWebBrowserAsync(WebBrowser webBrowser)
+        {
+
+            List<Tuple<string, string>> imagesList = new List<Tuple<string, string>>();
+
+            // Esegui uno script JavaScript per ottenere informazioni sulle immagini
+            var script = "var images = [];"
+                       + "var imgs = document.getElementsByTagName('img');"
+                       + "for (var i = 0; i < imgs.length; i++) {"
+                       + "  var img = imgs[i];"
+                       + "  var src = img.src;"
+                       + "  var alt = img.alt;"
+                       + "  images.push([src, alt]);"
+                       + "}"
+                       + "JSON.stringify(images);";
+
+            string result = webBrowser.Document.InvokeScript("eval", new object[] { script }) as string;
+
+            if (!string.IsNullOrEmpty(result))
+            {
+                // Deserializza il risultato JSON in una lista di tuple
+                imagesList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<string[]>>(result)
+                    .Select(x => Tuple.Create(x[0], x[1]))
+                    .ToList();
+            }
+
+            return imagesList;
+        }
+
+        int topMargin = 10;
+        int verticalSpacing = 10;
+        private async Task DisplayImagesAndAltTextAsync(Panel panel, List<Tuple<string, string>> imagesDictionary, string filter)
+        {
+            if (checkbox1.Checked)
+            {
+                panel.Controls.Clear();
+                topMargin = 10;
+            }
+
+            foreach (var entry in imagesDictionary)
+            {
+                if (entry.Item2.ToLower().Contains(filter))
+                {
+                    Image image = await DownloadImageAsync(entry.Item1);
+
+                    // Crea un PictureBox per l'immagine
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                    pictureBox.Image = image;
+                    pictureBox.Size = new Size(200, 150);
+                    pictureBox.Top = topMargin;
+
+                    // Crea un Label per il testo alt
+                    Label altLabel = new Label();
+                    altLabel.Text = entry.Item2;
+                    altLabel.AutoSize = true;
+                    altLabel.Top = topMargin + pictureBox.Height + verticalSpacing;
+
+                    // Aggiungi PictureBox e Label al panel
+                    panel.Controls.Add(pictureBox);
+                    panel.Controls.Add(altLabel);
+
+                    // Aggiorna la posizione per il prossimo set di immagine e testo alt
+                    topMargin += pictureBox.Height + altLabel.Height + verticalSpacing * 2;
+                }
+            }
+        }
+
+        private async Task<Image> DownloadImageAsync(string imageUrl)
+        {
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    byte[] data = await client.DownloadDataTaskAsync(new Uri(imageUrl));
+                    using (var ms = new System.IO.MemoryStream(data))
+                    {
+                        return Image.FromStream(ms);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Errore nel scaricare l'immagine: {ex.Message}");
+                    return null;
+                }
+            }
+        }
+    }
+}
